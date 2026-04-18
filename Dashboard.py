c0a3577628c4f914bceb766898db2c27ac3686bb
littlebuck4119 <thanapong@synaturegroup.com>
2026-04-18 23:58:38 +0700

    Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -30,7 +30,7 @@ st.markdown("""
 
 BRAND_CONFIG = {
     "Eat Am Are": "506e2020f13e6d515726",
-    "JonesSalad": "ใส่_ID_npoint_ของ_JonesSalad_ที่นี่", 
+    "JonesSalad": "695d80e67b2a8c1ca2ee", 
 }
 
 with st.sidebar:
