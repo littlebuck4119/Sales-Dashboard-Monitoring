0c33a179a96a0dd2d667e8592ca5fe1d6b2408e0
littlebuck4119 <thanapong@synaturegroup.com>
2026-04-24 16:14:08 +0700

    Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -26,6 +26,7 @@ BRAND_CONFIG = {
     "Eat Am Are": "506e2020f13e6d515726",
     "JonesSalad": "695d80e67b2a8c1ca2ee", 
     "Laem Charoen Seafood": "98d3735c3a0a94a513f6",
+    "MeationGroup": "90a9e466a623369dfac4"
 }
 CONFIG_API = "https://api.npoint.io/9898efa2a5853bf5f886"
 
