da4e9e289489347ae714106e48c8441b2b466f0c
littlebuck4119 <thanapong@synaturegroup.com>
2026-05-28 14:22:03 +0700

    Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -75,7 +75,8 @@ BRAND_CONFIG = {
     "You&I": "6111d22633f84f5ee575",
     "เสวย": "e7a4885887fc49db4765",
     "Shinkanzen": "5aae88055a5ff0d32c96",
-    "Tudari": "ead94150854efcbfe331"
+    "Tudari": "ead94150854efcbfe331",
+    "Spaghetti": "a1c5f81e3434b386e9b3"
 }
 CONFIG_API = "https://api.npoint.io/9898efa2a5853bf5f886"
 
